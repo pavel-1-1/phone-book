878c37c324956e1dfc403de387e41bf34c8f2495
Pavel <pavlik1123.pg@gmail.com>
2023-02-01 01:32:47 +0700

    Stub and tests for add

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -1,5 +1,9 @@
 package org.example;
 
+
 public class PhoneBook {
 
+    public boolean add(String name, int number) {
+        return false;
+    }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,3 +1,20 @@
-public class PhoneBookTest {
+import org.example.PhoneBook;
 
+import org.junit.Test;
+
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class PhoneBookTest {
+    PhoneBook book = new PhoneBook();
+    @Test
+    public void testAdd() {
+
+        boolean actual = book.add("Vasia", 800);
+
+        assertTrue(actual);
+
+    }
 }
