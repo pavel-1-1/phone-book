edcfe54a56298d1e9f5e1b30f1499043860ba592
Pavel <pavlik1123.pg@gmail.com>
2023-02-01 02:29:50 +0700

    add implemented

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -1,9 +1,21 @@
 package org.example;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PhoneBook {
+    private final List<String> names = new ArrayList<>();
+    private final List<Integer> numbers = new ArrayList<>();
 
     public boolean add(String name, int number) {
-        return false;
+        if (names.contains(name)) {
+            return false;
+        } else {
+            names.add(name);
+            numbers.add(number);
+        }
+        System.out.println(names.size());
+        return true;
     }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -4,17 +4,18 @@ import org.junit.Test;
 
 import java.util.Map;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class PhoneBookTest {
-    PhoneBook book = new PhoneBook();
+    PhoneBook bookTest = new PhoneBook();
+
     @Test
     public void testAdd() {
+        bookTest.add("pavel", 2323);
 
-        boolean actual = book.add("Vasia", 800);
+        boolean actual = bookTest.add("pavel", 2323);
 
-        assertTrue(actual);
+        assertFalse(actual);
 
     }
 }
